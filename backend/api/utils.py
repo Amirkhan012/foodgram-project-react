51952a924a6586b17bebeb0b5f585ba9fc4faff2
Amirkhan <ponezhev2001@mail.ru>
2023-01-23 12:56:40 +0300

    SPRINT 14: Try-3.5

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -12,7 +12,7 @@ def get_list_ingredients(user):
         recipe__cart_recipe__user=user).values(
         name=F('ingredient__name'),
         measurement_unit=F('ingredient__measurement_unit')
-    ).annotate(amount_ingredient=Sum('amount')).values_list(
-        'ingredient__name', 'amount', 'ingredient__measurement_unit'
+    ).annotate(amount__sum=Sum('amount')).values_list(
+        'ingredient__name', 'amount__sum', 'ingredient__measurement_unit'
     )
     return ingredients
