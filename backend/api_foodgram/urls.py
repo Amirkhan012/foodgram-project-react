cfa68a3a0c6d6e413f3fb35fc652eb9ff65e5e1d
Amirkhan <ponezhev2001@mail.ru>
2023-01-29 19:03:52 +0300

    try deploy 8

--- backend/api_foodgram/urls.py
+++ backend/api_foodgram/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('api/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
     path(
-        'redoc/',
+        'api/docs/redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
